261f5bf6f54d381e73e39fa0cd508d631211a621
Teo <teodorapop84@yahoo.com>
2023-09-16 07:09:00 -0700

    Delete + clear buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -31,7 +31,42 @@ public class Calculator implements ActionListener{
         textfield.setFont(myFont);
         textfield.setEditable(false);
 
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
 
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for(int i=0; i<8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i=0; i<10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50,430,145,50);
+        clrButton.setBounds(205,430,145,50);
+
+        frame.add(delButton);
+        frame.add(clrButton);
         frame.add(textfield);
         frame.setVisible(true);
 
